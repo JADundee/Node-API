ce9d785fdcac0b96d0acad3eb23cb318c2521d4e
JADundee <Jadundee@icloud.com>
2024-03-15 10:39:49 -0600

    Async CRUD operations

--- server.js
+++ server.js
@@ -38,7 +38,6 @@ app.use(cookieParser())
 
 // serve static files
 app.use('/', express.static(path.join(__dirname, '/public')))
-/* app.use('/subdir', express.static(path.join(__dirname, '/public'))) */
 
 // routes
 app.use('/', require('./routes/root'))
@@ -46,37 +45,8 @@ app.use('/register', require('./routes/register'))
 app.use('/auth', require('./routes/auth'))
 app.use('/refresh', require('./routes/refresh'))
 app.use('/logout', require('./routes/logout'))
-/* app.use('/subdir', require('./routes/subdir')) */
-
 app.use(verifyJWT)
 app.use('/employees', require('./routes/api/employees'))
-
-/* // route handlers
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('attempted to load hello.html')
-    next()
-}, (req, res) => {
-    res.send('Hello World!')
-}) */
-
-
-/* // chaining route handlers
-const one = (req, res, next) => {
-    console.log('one')
-    next()
-}
-
-const two = (req, res, next) => {
-    console.log('two')
-    next()
-}
-
-const three = (req, res, next) => {
-    console.log('three')
-    res.send('Finished')
-}
-
-app.get('/chain(.html)?', [one, two, three]) */
 
 app.all('*', (req, res) => {
     res.status(404)
